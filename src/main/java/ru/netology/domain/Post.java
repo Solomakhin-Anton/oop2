0c44274c1085d16381222731cdb555802d573784
Solomakhin Anton <s.anton45@yandex.ru>
2020-03-18 22:39:14 +0300

    Files added

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class Post {
+    private String postAuthor;
+    private int postTime;
+    private String imageUrl;
+    private int likeQuantity;
+    private int commentQuantity;
+    private int shareQuantity;
+    private int viewQuantity;
+    private String signature; //если есть подпись автора поста
+
+
+}
